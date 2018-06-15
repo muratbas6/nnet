3b97ea28641cbb793b8f4fba749e722418f8a21a
Murat Bas <alonebasketballer@gmail.com>
2018-06-15 22:53:41 +0300

    Update nn.py

--- neuralnetwork/nn.py
+++ neuralnetwork/nn.py
@@ -1,5 +1,12 @@
 import numpy as np
+import pandas as pd
 
+
+data = pd.read_csv('iris.csv',names=["x1","x2","x3","x4","y"])
+X = data.iloc[:,0:4]
+y = data.iloc[:,4:5].values
+print(X.shape)
+print(y)
 
 def nonlin(x, deriv=False):
     if (deriv == True):
@@ -8,31 +15,18 @@ def nonlin(x, deriv=False):
     return 1 / (1 + np.exp(-x))
 
 
-X = np.array([[5, 2, 2],
-              [4, 6, 1],
-              [1, 3, 2],
-              [2, 2, 3],
-              [2,7,1]])
-print(X.shape)
-
-
-y = np.array([[1],
-              [1],
-              [0],
-              [0],
-              [0]])
 
 np.random.seed(1)
 
 
-syn0 = 2 * np.random.randn(3, 4) - 1
+syn0 = 2 * np.random.randn(4, 4) - 1
 syn1 = 2 * np.random.randn(4, 1) - 1
 syn2 = 2*np.random.rand(1,4)-1
 syn3 = 2*np.random.rand(4,1)-1
 
 
 
-for j in range(600000):
+for j in range(100000):
 
 
     l0 = X
@@ -44,14 +38,13 @@ for j in range(600000):
     l4_error = y - l4
 
     if (j % 10000) == 0:
-        print("Error:" + str(np.mean(np.abs(l4_error))))
-        print(l4)
+        print(j,".Epoch ","Error:" + str(np.mean(np.abs(l4_error))))
 
     l4_delta = l4_error * nonlin(l4, deriv=True)
 
     l3_error = l4_delta.dot(syn3.T)
 
-    
+
     l3_delta = l3_error * nonlin(l3, deriv=True)
 
     l2_error=l3_delta.dot(syn2.T)
@@ -65,8 +58,8 @@ for j in range(600000):
     syn1 += l1.T.dot(l2_delta)/9
     syn0 += l0.T.dot(l1_delta)
 
-predict = np.array([5,3,1])
-l0=predict
+predict = np.array([[5.1,3.8,1.9,0.4]])
+l0 = predict
 l1 = nonlin(np.dot(l0, syn0))
 l2 = nonlin(np.dot(l1, syn1))
 l3 = nonlin(np.dot(l2,syn2))
